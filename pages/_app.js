7932c31f2c6b683ad202a49611fba82bc77fdc7d
AdrianRSolomon <adrian.r.solomon@gmail.com>
2022-10-30 02:52:46 -0400

    Added my project

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+/* eslint-disable @next/next/inline-script-id */
 import '../styles/globals.css';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import { StoreProvider } from '../utils/Store';
 import { SnackbarProvider } from 'notistack';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-
+import Script from 'next/script';
 const clientSideEmotionCache = createCache({ key: 'css' });
 
 function MyApp({
@@ -13,6 +14,21 @@ function MyApp({
   emotionCache = clientSideEmotionCache,
 }) {
   return (
+ <>
+    <Script
+    strategy='lazyOnLoad'
+    src={`https://www.googletagmanager.com/gtag/js?id= ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+      <Script 
+      strategy='lazyOnload'>
+         {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} );
+                `}
+      </Script>
+      
     <CacheProvider value={emotionCache}>
       <SnackbarProvider
         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
@@ -24,6 +40,7 @@ function MyApp({
         </StoreProvider>
       </SnackbarProvider>
     </CacheProvider>
+ </>
   );
 }
 
